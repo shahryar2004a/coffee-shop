60e89f3ca976070661ce2060d2cba6cdc51ca1d8
shahryar <shahryar.asgit@gmail.com>
2024-09-05 23:02:54 +0330

    import NavMobile.jsx to  Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './Nav/Nav'
 import Links from './Links/Links'
 import SVG from '../SVG/SVG'
+import NavMobile from './Nav/NavMobile/NavMobile'
 
 export default function Header() {
   return (
@@ -18,14 +19,15 @@ export default function Header() {
      <div className='flex md:hidden items-center justify-between h-16 px-4 bg-white dark:bg-zinc-700 '>
            
         
-           <SVG className='w-6 h-6 text-zinc-700 dark:text-white' name='bars'></SVG>
-        
-             
+             <SVG className='w-6 h-6 text-zinc-700 dark:text-white' name='bars'></SVG>
+              <NavMobile/>
+
             <SVG className='w-[100px] h-10 text-orange-300' name='logo-type'></SVG>
            
           
            <SVG className='w-6 h-6 text-zinc-700 dark:text-white' name='shopping-Cart'></SVG>
            
+          
      </div>
     </>
   )
