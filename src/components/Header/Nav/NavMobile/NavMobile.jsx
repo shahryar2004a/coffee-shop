66d5e8cbe63e0054223a95e54458b7a2b5241507
shahryar <shahryar.asgit@gmail.com>
2024-09-05 23:00:55 +0330

    create Component NavMobile.jsx

---
+++ src/components/Header/Nav/NavMobile/NavMobile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavMobile() {
+  return (
+    <>
+       <div className='fixed top-0 bottom-0 right-0 w-64 min-h-screen bg-white dark:bg-zinc-700 z-20'>
+
+        </div>
+
+        <div className='overlay fixed inset-0 w-full h-full bg-black/40 z-10'></div>
+    </>
+  )
+}
