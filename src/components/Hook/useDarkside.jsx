c9c5a6f2b1c5de0f3f938aa40834a64a93a6c4b8
shahryar <shahryar.asgit@gmail.com>
2024-09-05 19:13:32 +0330

    coding useDarkSide Hook

--- src/components/Hook/useDarkside.jsx
+++ src/components/Hook/useDarkside.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-export default function useDarkside() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function useDarkSide() {
+  const [theme, setTheme] = useState(localStorage.theme);
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    // save theme to local storage
+    localStorage.setItem('theme', theme);
+  }, [theme, colorTheme]);
+
+  return [colorTheme, setTheme];
 }
