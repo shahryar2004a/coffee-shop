97c2d07c7df363d825d5c55607cebe441ae35fb1
shahryar <shahryar.asgit@gmail.com>
2024-09-05 21:16:20 +0330

    responsive Nav.jsx and Header.jsx in size lg and md

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import Links from './Links/Links'
 export default function Header() {
   return (
     <>
-     <header className='fixed top-9 right-0 left-0 flex items-center w-[90%] h-24 bg-black/50 px-10 py-5 mx-auto rounded-3xl backdrop:blur-[6px]'>
+     <header className='fixed top-9 right-0 left-0 hidden md:flex items-center w-[98%] lg:w-[90%] h-24 bg-black/50 px-5 lg:px-10 py-5 mx-auto rounded-3xl backdrop:blur-[6px]'>
        <div className='flex justify-between items-center w-full'>
-      
+     
            <Nav/>
        
          <Links/>
