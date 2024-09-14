8f93ee4c526341fc45e11ef16ec483b3d9ff6e2c
shahryar <shahryar.asgit@gmail.com>
2024-09-14 16:25:37 +0330

    Create subMenu for NavMobile.jsx

--- src/components/Header/Nav/NavMobile/MenuNavMobile/MenuNavMobile.jsx
+++ src/components/Header/Nav/NavMobile/MenuNavMobile/MenuNavMobile.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import SVG from '../../../../SVG/SVG'
 
 export default function MenuNavMobile() {
+    
+    const subMenu=useRef()
+
+     const openSubMenu =(e) => {
+     e.target.parentElement.parentElement.classList.toggle('text-orange-300')
+       subMenu.current.classList.toggle('submenu--open')
+     }
   return (
     <>
          <div className='flex bg-orange-200/20 text-orange-300 mb-4 pr-2.5 h-10 rounded-md'>
@@ -11,14 +18,24 @@ export default function MenuNavMobile() {
               </a>
             </div>
            <ul className='child:pr-2.5 space-y-6 text-zinc-600 dark:text-white'>
-            <li className='flex items-center justify-between'>
+            <li>
+              <div className='flex items-center justify-between'>
               <a href="#" className='flex items-center gap-2'>
                 <SVG className='size-5' name='shopping-Cart'></SVG>
                  فروشگاه
               </a>
-              <span>
+              <span onClick={(e)=>openSubMenu(e)}>
               <SVG className='size-4' name='chevron-down'></SVG>
               </span>
+              </div>
+              <div ref={subMenu} className='submenu'>
+                            <a href="#">قهوه ویژه</a>
+                            <a href="#" className='submenu__item--active'>ویژه در سطح جهانی</a> 
+                            <a href="#">قهوه درجه یک</a>   
+                            <a href="#">ترکیبات تجاری</a> 
+                            <a href="#">کپسول قهوه</a>  
+                            <a href="#">قهوه زینو بریزیلی</a>
+              </div>
             </li>
             <li>
               <a href="#" className='flex items-center gap-2'>
