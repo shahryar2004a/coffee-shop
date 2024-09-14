e567f249156abd72bf01c869e0224c052a0ca594
shahryar <shahryar.asgit@gmail.com>
2024-09-14 17:05:30 +0330

    show & hide MenuMobile feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Nav from './Nav/Nav'
 import Links from './Links/Links'
 import SVG from '../SVG/SVG'
 import NavMobile from './Nav/NavMobile/NavMobile'
 
 export default function Header() {
+
+   const [showMenu,setShowMenu]=useState(false)
+
+   const openNavMobile=()=>{
+        setShowMenu(!showMenu)
+   }
   return (
     <>
      <header className='fixed top-9 right-0 left-0 hidden md:flex items-center w-[98%] lg:w-[90%] h-24 bg-black/50 px-5 lg:px-10 py-5 mx-auto rounded-3xl backdrop:blur-[6px]'>
@@ -16,11 +22,13 @@ export default function Header() {
        </div>
      </header>
      {/* header for mobile*/}
-     <div className='flex md:block items-center justify-between h-16 px-4 bg-white dark:bg-zinc-700 '>
+     <div className='flex md:hidden items-center justify-between h-16 px-4 bg-white dark:bg-zinc-700 '>
            
-        
-             <SVG className='w-6 h-6 text-zinc-700 dark:text-white' name='bars'></SVG>
-              <NavMobile/>
+            <div onClick={()=>openNavMobile()} className='nav-icon'>
+            <SVG className='w-6 h-6 text-zinc-700 dark:text-white' name='bars'></SVG>
+            </div>
+            
+              <NavMobile show={showMenu}/>
 
             <SVG className='w-[100px] h-10 text-orange-300' name='logo-type'></SVG>
            
--- src/components/Header/Nav/NavMobile/NavMobile.jsx
+++ src/components/Header/Nav/NavMobile/NavMobile.jsx
@@ -1,10 +1,14 @@
-import React,{useState} from 'react'
+import React,{useState,useRef} from 'react'
 import SVG from '../../../SVG/SVG'
 import useDarkSide from '../../../Hook/useDarkside'
 import { DarkModeSwitch } from 'react-toggle-dark-mode'
 import MenuNavMobile from './MenuNavMobile/MenuNavMobile'
 
-export default function NavMobile() {
+export default function NavMobile({show}) {
+
+   const menuMobilRef =useRef()
+   const overlayRef=useRef()
+
   const [colorTheme, setTheme] = useDarkSide();
   const [darkSide, setDarkSide] = useState(colorTheme === 'light' ? true : false);
 
@@ -12,16 +16,30 @@ export default function NavMobile() {
     setTheme(colorTheme);
     setDarkSide(checked);
   };
+
+  if(show)
+  {
+    menuMobilRef.current.classList.remove('-right-64')
+    menuMobilRef.current.classList.add('right-0')
+    overlayRef.current.classList.add('overlay--visible');
+  }
+  const closeMenuMobile= () => {
+    menuMobilRef.current.classList.add('-right-64')
+    menuMobilRef.current.classList.remove('right-0')
+    overlayRef.current.classList.remove('overlay--visible');
+  }
   return (
     <>
       
-       <div className=' font-YekanMedium fixed top-0 bottom-0 right-0 w-64 min-h-screen pt-3 px-4 bg-white dark:bg-zinc-700 z-20'>
+       <div ref={menuMobilRef} className=' font-YekanMedium fixed top-0 bottom-0 -right-64 w-64 min-h-screen pt-3 px-4 bg-white dark:bg-zinc-700 z-20 transition-all'>
          <div className='flex items-center justify-between pb-5 mb-6 border-b-gray-100 dark:border-b-white/10'>
           <div className='flex gap-x-3.5'>
           <SVG className='w-[41px] h-10 text-orange-300' name='logo'></SVG>
           <SVG className='w-[100px] h-10 text-orange-300' name='logo-type'></SVG>
           </div>
+          <div onClick={()=>closeMenuMobile()}>
           <SVG className='w-5 h-5 text-zinc-600 dark:text-white' name='x-mark'></SVG>
+          </div>
          </div>
 
          <MenuNavMobile/>
@@ -47,7 +65,7 @@ export default function NavMobile() {
          </div>
         </div>
 
-        <div className='overlay fixed inset-0 w-full h-full bg-black/40 z-10'></div>
+        <div onClick={()=>closeMenuMobile()} ref={overlayRef} className='overlay'></div>
     </>
   )
 }
