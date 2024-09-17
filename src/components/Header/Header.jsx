d71e3cbfd2ac72b3de09dc7694d2c31503d30084
shahryar <shahryar.asgit@gmail.com>
2024-09-17 19:58:13 +0330

    main structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,6 +20,7 @@ export default function Header() {
    }
   return (
     <>
+           {/* App Header */}
      <header className='fixed top-9 right-0 left-0 hidden md:flex items-center w-[98%] lg:w-[90%] h-24 bg-black/50 px-5 lg:px-10 py-5 mx-auto rounded-3xl backdrop:blur-[6px]'>
        <div className='flex justify-between items-center w-full'>
      
@@ -47,7 +48,27 @@ export default function Header() {
            {/* cart modal mobile */}
               {/*cart */}
              <CartModalMobile showModal={showModalCart}/>
+ 
+
+             
+               
+
+
      </div>
+
+           {/* App Main */}
+         <main>
+          <section className='home '>
+            <div className='container md:min-h-screen flex justify-end items-center'>
+             <div className='text-white'>
+              <h2 className='font-YekanHeavy text-2xl md:text-6xl mb-0.5 md:mb-2'>قهوه عربیکا تانزانیا</h2>
+              <span className='font-Yekan text-xl md:text-5xl'>یک فنجان بالانس </span>
+              <span className='block w-[100px] h-px md:h-0.5 bg-orange-300 my-3 md:my-8'></span>
+              <p className='font-YekanRegular max-w-[201px] md:max-w-[460px] text-xs md:text-2xl'>قطعا نام آشنای عربیکا را شنیده اید،عربیکا یکی از گونه های قهوه است که در نواحی مختلف کمربند قهوه کشت میشود</p>
+             </div>
+            </div>
+          </section>
+         </main>
     </>
   )
 }
