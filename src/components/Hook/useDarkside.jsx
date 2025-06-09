174d9467c55d3a9c5a735af73da091a831019fdd
shahryar <shahryar.asgit@gmail.com>
2025-06-09 19:00:36 +0330

    add Darkside.jsx

--- src/components/Hook/useDarkside.jsx
+++ src/components/Hook/useDarkside.jsx
@@ -8,7 +8,7 @@ export default function useDarkSide() {
     const root = window.document.documentElement;
     root.classList.remove(colorTheme);
     root.classList.add(theme);
-
+    
     // save theme to local storage
     localStorage.setItem('theme', theme);
   }, [theme, colorTheme]);
